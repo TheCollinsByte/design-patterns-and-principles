7e678f0626865db409c60d31b15a2687088abb57
colspan <collin@collindigm.com>
2024-11-13 23:58:04 +0300

    Test Environment: Initialized

---
+++ principles/src/test/java/com/thecollinsbyte/solid/singlerepository/AccountOperationsTest.java
@@ -0,0 +1,25 @@
+package com.thecollinsbyte.solid.singlerepository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountOperationsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void addAccount() {
+    }
+
+    @Test
+    void updateAccount() {
+    }
+
+    @Test
+    void getAccount() {
+    }
+}
---
+++ principles/src/test/java/com/thecollinsbyte/solid/singlerepository/TransactionOperationsTest.java
@@ -0,0 +1,36 @@
+package com.thecollinsbyte.solid.singlerepository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TransactionOperationsTest {
+
+    Account account;
+    TransactionOperations transactionOperations;
+
+    @BeforeEach
+    void setUp() {
+        account = new Account();
+        account.setAccountNumber(1234);
+        account.setFirstName("John");
+        account.setTotalAmount(BigDecimal.valueOf(5000));
+
+        transactionOperations = new TransactionOperations(account);
+    }
+
+    @Test
+    void deposit() {
+        transactionOperations.deposit(BigDecimal.valueOf(5000), 1234);
+        assertEquals(account.getTotalAmount(), new BigDecimal(10000));
+    }
+
+    @Test
+    void withdraw() {
+        transactionOperations.withdraw(BigDecimal.valueOf(2000), 1234);
+        assertEquals(account.getTotalAmount(), new BigDecimal(3000));
+    }
+}
